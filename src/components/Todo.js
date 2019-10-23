5e5a3881de16525e09ff7fb3d3309d688e491a9b
joseph townshend <joetownshend@mac.com>
2019-10-23 13:51:44 +0100

    feat: adds line through to new todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
-export default props => <div>{props.text}</div>;
+export default props => (
+  <div
+    style={{
+      textDecoration: 'line-through'
+    }}
+    onClick={props.toggleComplete}
+  >
+    {props.todo.text}
+  </div>
+);
