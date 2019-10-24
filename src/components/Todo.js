a0ed9a29973397b8b2e4d40a4b79c7071913bc97
joseph townshend <joetownshend@mac.com>
2019-10-24 12:42:39 +0100

    feat: line-through condional on complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default props => (
   <div
     style={{
-      textDecoration: 'line-through'
+      textDecoration: props.todo.complete ? 'line-through' : ""
     }}
     onClick={props.toggleComplete}
   >
