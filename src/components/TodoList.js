e3ad53cbe70102f6b6f3db62a104bf81123299ff
joseph townshend <joetownshend@mac.com>
2019-10-24 12:47:43 +0100

    docs: update MVC

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -40,7 +40,9 @@ export default class TodoList extends React.Component {
             todo={todo}
           />
         ))}
-        <div>active todos: {this.state.todos.filter(todo => !todo.complete).length}</div>
+        <div>
+          active todos: {this.state.todos.filter(todo => !todo.complete).length}
+        </div>
       </div>
     );
   }
